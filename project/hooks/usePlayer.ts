41038242c6c04ececbf8a34efed7a340adf3dce3
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-12-27 02:38:21 -0500

    In Progress

--- project/hooks/usePlayer.ts
+++ project/hooks/usePlayer.ts
@@ -1,19 +1,23 @@
 import { create } from "zustand";
 
 interface PlayerStore {
-  ids: string[]; // Corrected property name from 'id' to 'ids'
+  ids: string[];
   activeId?: string;
+  isShuffle: boolean; // New property to track shuffle mode
   setId: (id: string) => void;
   setIds: (ids: string[]) => void;
+  toggleShuffle: () => void; // Function to toggle shuffle mode
   reset: () => void;
 }
 
 const usePlayer = create<PlayerStore>((set) => ({
-  ids: [], // Corrected property name from 'id' to 'ids'
+  ids: [],
   activeId: undefined,
+  isShuffle: false, // Initialize shuffle mode to false
   setId: (id: string) => set({ activeId: id }),
   setIds: (ids: string[]) => set({ ids: ids }),
-  reset: () => set({ ids: [], activeId: undefined }),
+  toggleShuffle: () => set((state) => ({ isShuffle: !state.isShuffle })), // Toggle shuffle mode
+  reset: () => set({ ids: [], activeId: undefined, isShuffle: false }), // Reset shuffle mode on reset
 }));
 
 export default usePlayer;
